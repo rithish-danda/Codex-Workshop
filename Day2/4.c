ad32c5f42781292da9b5c0414b5533aedc9f69a4
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2023-02-27 23:04:06 +0530

    Create 4.c

---
+++ Day2/4.c
@@ -0,0 +1,16 @@
+//GUESS THE OUTPUT
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(){
+
+  int x= 2;
+  printf("%d",x);
+  int y=++x*++x(x++**++x);
+  printf("%d",x);
+  printf("%d",y);
+  x==y==2;
+  printf("%d",x);
+  return 0;
+}
